8b83f86d5463d0cb753d857f43af2b8e48674a54
devAstov7 <atomg0102@gmail.com>
2026-09-04 02:21:15 -0300

    Update password and hash values in hash.js

--- backend/hash.js
+++ backend/hash.js
@@ -1,8 +1,8 @@
 import bcrypt from "bcryptjs";
 
-const senha = "RoTaadMin08191@gpj#";
+const senha = "senha";
 
-const hash = "$2b$12$ERrTeqKTb6Vh/5U7xmpbouD7g/90idv0NjRMDBPD6.RCcm4dNoJQu";
+const hash = "senhahash";
 
 const resultado = await bcrypt.compare(senha, hash);
 
